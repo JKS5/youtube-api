bf3b83bff2a0db5634242d8f5eb45fa8dfbd7454
JKS5 <jokekhanson@gmail.com>
2023-04-12 21:38:51 +0900

    [videoCard만들기]

--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
   const { keyword } = useParams();
 
-  const { isLoading,isError,error, isSuccess, data } = useQuery('Videos', ()=>{
-    fetch(`popular.json`)
+  const {
+    isLoading,
+    isError,
+    error,
+    isSuccess,
+    data: videos, //여기에 들어감.
+  } = useQuery(["vidoes", keyword], async () => {
+    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((response) => response.json())
+      .then((data) => data.items); // data의 items를 return 해서 위
   });
 
   console.log(keyword);
-  return <div>Videos {keyword ? `${keyword}searched` : "popular"}</div>;
+  return (
+    <div>
+      <div>Videos {keyword ? `${keyword} searched!!` : "popular!"}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is Wrong !!!!!!!!!!!</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            // <VideoCard key={video.id} video={video} />
+            <div key={video.id}>{video.snippet.title}</div>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
