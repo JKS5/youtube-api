f40e57062664389ee8f7b24a0a38a217afef9716
JKS5 <jokekhanson@gmail.com>
2023-04-28 16:10:16 +0900

    fixtypo

--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -29,8 +29,6 @@ export default function Videos() {
           ))}
         </ul>
       )}
-
-      <div>Videos</div>
     </div>
   );
 }
--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -23,7 +23,6 @@ export default function RelatedVideos({ id }) {
           ))}
         </ul>
       )}
-      <p>slsajflksjldflkjfljdslafdjdf</p>
     </>
   );
 }
